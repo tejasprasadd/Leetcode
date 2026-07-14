9d7f082453b9e3b9e5f8224fe781dc5ad7551e56
B R Tejas Prasad <tejasprasad43@gmail.com>
2026-07-14 12:11:45 +0530

    Finished 3Sum

--- src/ThreeSum_2.java
+++ src/ThreeSum_2.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import java.util.*;
 
-public class ThreeSum_2 {
+//Hashmap solution
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+        for (int i = 0; i < n; i++) {
+            count.put(nums[i], count.get(nums[i]) - 1);
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < n; j++) {
+                count.put(nums[j], count.get(nums[j]) - 1);
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int target = -(nums[i] + nums[j]);
+                if (count.getOrDefault(target, 0) > 0) {
+                    result.add(Arrays.asList(nums[i], nums[j], target));
+                }
+            }
+            for (int j = i + 1; j < nums.length; j++) {
+                count.put(nums[j], count.get(nums[j]) + 1);
+            }
+        }
+
+        return result;
+    }
+}
+
+void main(String[] args) {
+    Solution solution = new Solution();
+    int[] nums = {-1, 0, 1, 2, -1, -4};
+    List<List<Integer>> result;
+    result = solution.threeSum(nums);
+    System.out.println(result);
 }
