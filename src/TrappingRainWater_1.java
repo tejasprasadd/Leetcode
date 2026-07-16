81ba0d9d70d302288a03354a06b48a949ce2327e
B R Tejas Prasad <tejasprasad43@gmail.com>
2026-07-16 16:37:01 +0530

    Finished Trapping Rain water in 2 solutions

---
+++ src/TrappingRainWater_1.java
@@ -0,0 +1,32 @@
+
+//Brute force solution
+public class Solution {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int n = height.length;
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+            int leftMax = height[i];
+            int rightMax = height[i];
+
+            for (int j = 0; j < i; j++) {
+                leftMax = Math.max(leftMax, height[j]);
+            }
+            for (int j = i + 1; j < n; j++) {
+                rightMax = Math.max(rightMax, height[j]);
+            }
+
+            res += Math.min(leftMax, rightMax) - height[i];
+        }
+        return res;
+    }
+}
+void main(String[] args) {
+    Solution solution = new Solution();
+    int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+    int result = solution.trap(height);
+    System.out.println(result);
+}
---
+++ src/TrappingRainWater_2.java
@@ -0,0 +1,31 @@
+
+//Two Pointer  solution
+public class Solution {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int n = height.length, result = 0, left = 0, right = n - 1;
+        int leftMax =height[left], rightMax= height[right];
+        while(left< right){
+            if(leftMax< rightMax){
+                left++;
+                leftMax=Math.max(leftMax,height[left]);
+                result+=leftMax-height[left];
+            }
+            else{
+                right--;
+                rightMax=Math.max(rightMax,height[right]);
+                result+=rightMax-height[right];
+            }
+        }
+        return result;
+    }
+}
+
+void main(String[] args) {
+    Solution solution = new Solution();
+    int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+    int result = solution.trap(height);
+    System.out.println(result);
+}
