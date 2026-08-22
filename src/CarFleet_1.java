cf8b049949de8efb2708014c9b7a116a097af1d5
B R Tejas Prasad <tejasprasad43@gmail.com>
2026-08-22 14:39:50 +0530

    Finished Car Fleet in 2 solutions

---
+++ src/CarFleet_1.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+class CarFleet_1 {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int[][] pair = new int[position.length][2];
+        for (int i = 0; i < position.length; i++) {
+            pair[i][0] = position[i];
+            pair[i][1] = speed[i];
+        }
+        Arrays.sort(pair, (a, b) -> Integer.compare(b[0], a[0]));
+        Stack<Double> stack = new Stack<>();
+        for (int[] p : pair) {
+            stack.push((double) (target - p[0]) / p[1]);
+            if (stack.size() >= 2 && stack.peek() <= stack.get(stack.size() - 2)) {
+                stack.pop();
+            }
+        }
+        return stack.size();
+    }
+
+    static void main(String[] args) {
+        CarFleet_2 solution = new CarFleet_2();
+        int target = 12;
+        int[] position = {10, 8, 0, 5, 3};
+        int[] speed = {2, 4, 1, 1, 3};
+        int result = solution.carFleet(target, position, speed);
+        System.out.println(result);
+
+    }
+}
