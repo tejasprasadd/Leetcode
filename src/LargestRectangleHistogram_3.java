6709c183a4c18c6de895a3e277dead08cfb9ff16
B R Tejas Prasad <tejasprasad43@gmail.com>
2026-09-06 18:51:33 +0530

    Finished Largest Rectangle in Histogram

--- src/LargestRectangleHistogram_3.java
+++ src/LargestRectangleHistogram_3.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Stack;
 
-public class LargestRectangleHistogram_3 {
+public static class LargestRectangleinHistogram_3 {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int maxArea = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i <= n; i++) {
+            while (!stack.isEmpty() && (i == n || heights[stack.peek()] >= heights[i])) {
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+        return maxArea;
+    }
 }
+
+public static void main(String[] args) {
+    LargestRectangleinHistogram_3 solution = new LargestRectangleinHistogram_3();
+    int[] heights = {2, 1, 5, 6, 2, 3};
+    int result = solution.largestRectangleArea(heights);
+    System.out.println(result);
+}
+
--- src/LargestRectangleinHistogram_1.java
+++ src/LargestRectangleinHistogram_1.java
@@ -1,2 +1,32 @@
 public class LargestRectangleinHistogram_1 {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+            int height = heights[i];
+
+            int rightMost = i + 1;
+            while (rightMost < n && heights[rightMost] >= height) {
+                rightMost++;
+            }
+
+            int leftMost = i;
+            while (leftMost >=0 && heights[leftMost] >= height) {
+                leftMost--;
+            }
+            //These 2 lines beloew is present because the while loop shoots the values to the first invalid respective value and hence we are moving back to that last valid one
+            rightMost--;
+            leftMost++;
+            maxArea = Math.max(maxArea, height * (rightMost - leftMost + 1));
+        }
+        return maxArea;
+    }
+
+    static void main(String[] args) {
+        LargestRectangleinHistogram_1 solution = new LargestRectangleinHistogram_1();
+        int[] heights = {2, 1, 5, 6, 2, 3};
+        int result = solution.largestRectangleArea(heights);
+        System.out.println(result);
+    }
 }
